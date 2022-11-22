766db2183509b6d3eafb139099e3eda733ab993f
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2022-11-22 19:31:15 -0300

    Adicionando Get e Post

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 hoteis = [
     {
@@ -9,7 +9,7 @@ hoteis = [
         'cidade': 'Mogi das Cruzes'
     },
     {
-            'hotel_id': 'beta',
+        'hotel_id': 'beta',
         'nome': 'Beta Hotel',
         'estrelas': 3.5,
         'diaria': 30,
@@ -26,4 +26,47 @@ hoteis = [
 class Hoteis(Resource): 
     def get(self):
         return {'hoteis': hoteis}
+
+class Hotel(Resource):
+    
+    def get(self, hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return {'message': 'Hotel not found.'}, 404 
+    
+    def post(self, hotel_id):
         
+        #selecionar os argumentos passados no json e colocar em uma variavel
+        argumentos = reqparse.RequestParser()
+        argumentos.add_argument('nome')
+        argumentos.add_argument('estrelas')
+        argumentos.add_argument('diaria')
+        argumentos.add_argument('cidade')
+        
+        #passando os valores para outra variável
+        dados = argumentos.parse_args()
+        
+        #formando um novo hotel
+        new_hotel = {
+            'hotel_id': hotel_id,
+            'nome': dados['nome'],
+            'estrelas': dados['estrelas'],
+            'diaria': dados['diaria'],
+            'cidade': dados['cidade']
+        }
+        
+        #adicionando o hotel na lista de hoteis
+        hoteis.append(new_hotel)
+        
+        #devolve o novo hotel caso de 200
+        return new_hotel, 200
+        
+    def put(self, hotel_id):
+        pass
+    
+    def delete(self, hotel_id):
+        pass
+    
+    
+    
